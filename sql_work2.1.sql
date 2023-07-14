87a8ceffeda951bd1ceab91f1674a8f1ccb5cce5
prajakta1611 <gawasprachi95@gmail.com>
2023-07-14 23:28:45 +0530

    update

---
+++ sql_work2.1.sql
@@ -0,0 +1,14 @@
+-- Q1 To check length of the string.
+SELECT length("HELLO WORLD");
+
+-- Q2 To make string lowercase
+SELECT LOWER("HELLO WORLD");
+
+-- Q3 To make string upperCase
+SELECT UPPER("hellO WORLD");
+
+-- Q4 Concat two string 
+SELECT CONCAT("FIRST"," ","LAST");
+
+-- Q5 To replace the word
+SELECT REPLACE("HELLO","HELLO","HII");
