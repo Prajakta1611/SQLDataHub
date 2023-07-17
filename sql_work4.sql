1c4cd4ae1082ea7f08cc3deb07bc39e143f050ae
prajakta1611 <gawasprachi95@gmail.com>
2023-07-17 17:05:09 +0530

    update

---
+++ sql_work4.sql
@@ -0,0 +1,14 @@
+-- ROUND FUNCTION
+select round(22.8);
+
+-- FLOOR  FUNCTION
+SELECT floor(3.3);
+
+-- CEILING FUNCTION
+SELECT ceiling(3.3);
+
+-- TRUNCATR FUNCTION
+SELECT truncate(3.6663,1);
+
+-- ABS FUNCTION 
+SELECT abs(-99);
